933f9a356fb0ab17f45a2f6301484da0020425ca
root <root@DESKTOP-6B3FQL8.localdomain>
2022-06-24 08:56:28 -0500

    finished

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,1 +1,34 @@
 "use strict";
+
+// TODO 1:
+let chocolateBars =['snickers','hundred grand','kitkat','skittles']
+
+// TODO 2:
+let names = ['James',"Jack"]
+names = [...names,'John']
+
+let numbers = [25,26,27]
+numbers[22]= 22
+
+// TODO 3:
+let peopleSize = ['S','M']
+peopleSize = ['L',...peopleSize]
+
+let seconds = [35,34,33]
+seconds[37]= 37
+
+// TODO 4:
+let dayOfTheWeek = ['Thrusday',"Friday","Satarday"]
+dayOfTheWeek.shift()
+
+// TODO 5:
+let fruit = ['Pants','Apples','Bananas']
+fruit.slice(1)
+
+// TODO 6:
+let pets = ['dogs','cats','fish','human']
+pets.pop()
+
+// TODO 7:
+let pants = ['black','white','apple']
+pants.slice(0, pants.length -1)
--- loops/loops.js
+++ loops/loops.js
@@ -5,27 +5,38 @@
 
 // 1) Get the sum of two arrays…actually the sum of all their elements.
 // P.S. Each array includes only integer numbers. Output is a number too.
-
+function sumOfTwo(){
 let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
 let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
 // Example output: 
 // 276 + 351 = 627
-
+let sum = 0;
+for(let i =0; i<arr_1;i++){
+    sum+= arr_1 + arr_2;
+}
+}
+sumOfTwo()
+console.log(sum);
 
 // 2) Using a for loop print all even numbers up to and including n. Don’t include 0.
-
-let n1 = 22; 
+function value(){
+let n1 = 22;
 // Example output: 
 // 2 4 6 8 10 12 14 16 18 20 22 OR each item on a new line
- 
-
+for(let i = 2;i<=n1;i+=2){
+console.log(`Itreation is #${i}`);
+}
+}
+value()
 
 // 3) Using a for loop output the elements in reverse order
 
 let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
 // Example output: 
 // true 3.5  be  false cannot  true 9 what 43 OR each item on a new line
-
+for(let i = 0; i<arr.length;i++){
+arr.reverse();
+}
 
 
 
@@ -37,7 +48,12 @@ let arr_3   = [4, 6, 7];
 let arr_4    = [8, 1, 9];
 // Example output: 
 // [12, 7, 16]
+var result = [];
 
+for (var i = 0; i < arr_3.length; i++) {
+    result.push(arr_3[i] + arr_4[i] );
+}
+result;
 
 
 
@@ -45,8 +61,85 @@ let arr_4    = [8, 1, 9];
 
 // Sample numbers : 0, -1, 4
 // Output : 4, 0, -1
-
+function order(){
+let a= 0;
+let b=-1;
+let c= 4;
+if (a>b && a>c)
+{
+        if (b>c)
+        {
+            alert(a + ", " + b + ", " +c)
+            console.log(a + ", " + b + ", " +c);
+        }
+        else
+        {
+            alert(a + ", " + c + ", " +b)
+            console.log(a + ", " + c + ", " +b);
+        }
+}
+else if (b>a && b >c)
+{
+        if (a>c)
+        {
+            alert(b + ", " + a + ", " +c)
+             console.log(b + ", " + a + ", " +c);
+        }
+        else
+        {
+            alert(b + ", " + c + ", " +a)
+             console.log(b + ", " + c + ", " +a);
+        }
+}
+else if (c>a && c>b)
+{
+        if (a>b)
+        {
+            alert(c + ", " + a + ", " +b)
+            console.log(c + ", " + a + ", " +b);
+        }
+        else
+        {
+            alert(c + ", " + b + ", " +a)
+            console.log(c + ", " + b + ", " +a);
+        }
+}
+}
+order()
 
 // 6) Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result. Go to the editor
 // Sample numbers : -5, -2, -6, 0, -1
 // Output : 0
+function highest(){
+    a=-5;
+b=-2;
+c=-6;
+d= 0;
+f=-1;
+if (a>b && a>c && a>d && a>f)
+{
+    alert(a)
+    console.log(a);
+}
+else if (b>a && b>c && b>d && b>f)
+{
+    alert(b)
+    console.log(b);
+}
+else if (c>a && c>b && c>d && c>f)
+{
+    alert(c)
+    console.log(c);
+}
+else if (d>a && d>c && d>b && d>f)
+{
+    alert(d)
+    console.log(d);
+}
+else
+{
+    alert(f)
+    console.log(f);
+}
+}
+highest()
